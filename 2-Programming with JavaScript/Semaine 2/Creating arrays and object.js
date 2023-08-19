2a1b9a17e1e6906ff601e2daf7d9dc43b43cc787
Your GitHub username <Your GitHub email>
2023-08-19 21:51:05 +0000

    Add Js exercices

---
+++ 2-Programming with JavaScript/Semaine 2/Creating arrays and object.js
@@ -0,0 +1,51 @@
+/*
+Exercise: Creating arrays and objects
+In this exercise lab you will practice creating arrays and objects.
+
+Tasks to complete
+Create a new empty array literal and assign it to the variable clothes.
+
+Add 5 of your favorite items of clothing as strings using the push() method.
+
+Remove the fifth piece of clothing from the array using the pop() method.
+
+Add a new piece of clothing using the push() method.
+
+Use console.log to show the third item from the clothes array in the console.
+
+Create a new empty object literal and assign it to the variable favCar.
+
+Using the dot notation, assign a color property to the favCar object and give it a string value with the color of your choice.
+
+Using the dot notation, assign a covertible property to the favCar object and give it a boolean value of your choice.
+
+Use the console to log the entire favCar object.
+*/
+
+
+var clothes = [];
+
+clothes.push("T-shirt", "Pair of jeans", "Penttalon", "Jacket", "Null")
+clothes.pop() // delete the last item
+clothes.push("Shirt")
+
+console.log(clothes[3]);
+
+var favCar = {};
+
+favCar.color = "Green"
+favCar.convertible = true
+
+console.log(favCar);
+
+// Creating arrays and objects (solutions)
+
+var clothes = [];
+clothes.push('gray t-shirt'); // 1st item of clothing
+clothes.push('blue t-shirt'); // 2nd item of clothing
+clothes.push('yellow t-shirt'); // 3rd item of clothing
+clothes.push('slippers'); // 4th item of clothing
+clothes.push('old jeans'); // 5th item of clothing
+clothes.pop();
+clothes.push('green scarf');
+console.log(clothes[2]);
